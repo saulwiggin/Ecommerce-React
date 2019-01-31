57506780d17c464c4c14633428bc678b0d0d02b8
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2019-01-31 09:26:42 +0000

    Add expansion panels to display delivery options

--- src/views/Basket/Basket.jsx
+++ src/views/Basket/Basket.jsx
@@ -30,6 +30,13 @@ import CardIcon from "components/Card/CardIcon.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
 
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import {CardElement, injectStripe} from 'react-stripe-elements';
 import CheckoutForm from "components/Stripe/CheckoutForm"
 
@@ -45,10 +52,34 @@ import {
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
 
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+});
+
 class Basket extends React.Component {
   state = {
-    value: 0
+    value: 0,
+    expanded: null,
   };
+
+  handleAccordianChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+
+
   handleChange = (event, value) => {
     this.setState({ value });
   };
@@ -169,7 +200,7 @@ class Basket extends React.Component {
     return (
       <div>
       <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={6} sm={6} md={3}>
           <Card>
             <form>
               <div data-locale-reversible>
@@ -224,6 +255,67 @@ class Basket extends React.Component {
                 </svg>
                 <span class="message"></span></div>
             </form>
+            </Card>
+          </GridItem>
+          <GridItem xs={6} sm={6} md={3}>
+            <Card>
+              <CardHeader>
+                <h2> Checkout Basket </h2>
+              </CardHeader>
+              <CardBody>
+                <h2> Delivery Options </h2>
+                <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleAccordianChange('panel1')}>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>General settings</Typography>
+                    <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                      maximus est, id dignissim quam.
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleAccordianChange('panel2')}>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>Users</Typography>
+                    <Typography className={classes.secondaryHeading}>
+                      You are currently not an owner
+                    </Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
+                      diam eros in elit. Pellentesque convallis laoreet laoreet.
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleAccordianChange('panel3')}>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>Advanced settings</Typography>
+                    <Typography className={classes.secondaryHeading}>
+                      Filtering has been entirely disabled for whole web server
+                    </Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
+                      eros, vitae egestas augue. Duis vel est augue.
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel expanded={expanded === 'panel4'} onChange={this.handleAccordianChange('panel4')}>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>Personal data</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
+                      eros, vitae egestas augue. Duis vel est augue.
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              </CardBody>
             </Card>
           </GridItem>
         </GridContainer>
